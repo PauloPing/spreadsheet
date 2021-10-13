9732504c18b39621c1ac0a02e0d2b0c19c39bfa2
Louis Vanoise <louisv.digit@gmail.com>
2021-10-13 09:28:41 +0200

    test_function

---
+++ function.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#include <math.h>
+
+produit ()
+{
+    res=`$1 * $2`
+    return res
+}
+
+quotient ()
+{
+    res=`expr $1\/$2`
+    return res
+}
+
+puissance ()
+{
+    res=$1
+    while test $2 -ne 0 
+    do
+        res=`expr $res * $1`
+    done
+    return res
+}
+
+ln ()
+{
+    a=$1
+    res=$(bc -l <<<"l($a)")
+    return res
+}
+
+exp ()
+{
+    a=$1
+    res=$(bc -l <<<"e(25)")
+    return res
+}
+
+sqrt ()
+{
+    a=$1
+    res=$(bc -l <<<"sqrt($a)")
+    return res
+}
