2de08f58f97e1c522b72d01be3ef7e2f465e2dc7
LEBLOND Paul <polo62.leblond@gmail.com>
2021-11-14 16:18:54 +0100

    feat(tableur) : param new version + verification type of params

--- tableur.sh
+++ tableur.sh
@@ -2,12 +2,8 @@
 
 # Groupe de 2   : Louis et Paul 
 
-feuille=0
-resultat=0
-scinSep=0
-slinSep=0
-scoutSep=0
-sloutSep=0
+scinSep="\t"
+slinSep="\n"
 inverse=0
 
 compteur=0
@@ -21,7 +17,6 @@ do
       if [[ -f $var ]]
       then
         feuille=$var
-        echo "-in feuille ==> $feuille"
       else 
         echo "Param for -in is not a file, please enter a valid param or remove it!" && exit 1
       fi
@@ -30,26 +25,21 @@ do
       if [[ -f $var ]]
       then
         resultat=$var
-        echo "-out feuille ==> $resultat"
         else 
         echo "Param for -out is not a file, please enter a valid param or remove it!" && exit 1
       fi
     elif [[ ${!compteur} == "-scin" ]]
     then 
       scinSep=$var
-      echo "-scin sep ==> $scinSep"
     elif [[ ${!compteur} == "-slin" ]]
     then 
       slinSep=$var
-      echo "-slin sep ==> $slinSep"
     elif [[ ${!compteur} == "-scout" ]]
     then 
       scoutSep=$var
-      echo "-scout sep ==> $scoutSep"
     elif [[ ${!compteur} == "-slout" ]]
     then 
       sloutSep=$var
-      echo "-slout sep ==> $sloutSep"
     fi
   fi
   compteur=$(($compteur + 1));
@@ -58,5 +48,22 @@ done
 if [[ ${!compteur} == "-inverse" ]]
 then 
   inverse=1
-  echo "-inverse ==> $inverse"
 fi
+
+if [[ ! $scoutSep ]]
+then 
+  scoutSep=$scinSep
+fi
+
+if [[ ! $sloutSep ]]
+then 
+  sloutSep=$slinSep
+fi
+
+echo "feuille : $feuille"
+echo "resultat : $resultat"
+echo "scinSep : $scinSep"
+echo "slinSep : $slinSep"
+echo "scoutSep : $scoutSep"
+echo "sloutSep : $sloutSep"
+echo "inverse : $inverse"
